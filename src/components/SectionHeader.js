c059a811449c0848259bcd7c4fb0e625c1c890f7
Brijen Makwana <brijenma@gmail.com>
2023-01-22 18:47:18 +0530

    SectionHeader

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./SectionHeader.css";
 
-export default function SectionHeader() {
-  return <div>SectionHeader</div>;
+export default function SectionHeader(props) {
+  const { heading, description } = props;
+  return (
+    <div className="section-header-container">
+      <h2 className="section-heading">{heading}</h2>
+      <h4 className="section-description">{description}</h4>
+    </div>
+  );
 }
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+const sectionData = [
+  {
+    heading: "Introduction",
+    description: "Introduce yourself. Tell visitors about you and who you are.",
+  },
+  {
+    heading: "Skills",
+    description:
+      "Show off the languages, frameworks, software and tech that you use.",
+  },
+  {
+    heading: "Socials",
+    description: "Connect with your visitors by adding links to your socials.",
+  },
+  {
+    heading: "Badges",
+    description: "Add some badges and stats to your profile.",
+  },
+  {
+    heading: "Support",
+    description:
+      "Make it easy for people using your products to support you or give donations.",
+  },
+];
+
+export { sectionData };
